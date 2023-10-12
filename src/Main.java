cac135981bff96e2e6666d616d82f0bcf0b478ff
Fortisek <kubek05w@gmail.com>
2023-10-12 08:13:03 +0200

    first commmit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public class klasa {
+
+        private  String imie;
+
+        private  String nazwisko;
+
+        public ObiektA(String imie, String nazwisko){
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public String getImie(){
+            return imie;
+        }
+
+    }
+}
